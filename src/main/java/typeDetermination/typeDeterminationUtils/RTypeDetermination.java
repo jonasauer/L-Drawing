0fa02d1cd31fd14505f11441756880b8d791c41a
Jonas Auer <auerjonas95@gmail.com>
2018-01-07 23:45:25 +0100

    Added all typeDetermination classes, but s,p,r are still not implemented

---
+++ src/main/java/typeDetermination/typeDeterminationUtils/RTypeDetermination.java
@@ -0,0 +1,16 @@
+package main.java.typeDetermination.typeDeterminationUtils;
+
+import main.java.decomposition.graph.DirectedEdge;
+import main.java.decomposition.hyperGraph.Vertex;
+import main.java.decomposition.spqrTree.TCTree;
+import main.java.decomposition.spqrTree.TCTreeNode;
+
+public class RTypeDetermination implements ITypeDetermination{
+
+
+    @Override
+    public void determineType(TCTree<DirectedEdge, Vertex> tcTree, TCTreeNode<DirectedEdge, Vertex> tcTreeNode) {
+
+        //TODO: implement method
+    }
+}
---
+++ src/main/java/typeDetermination/typeDeterminationUtils/STypeDetermination.java
@@ -0,0 +1,18 @@
+package main.java.typeDetermination.typeDeterminationUtils;
+
+import main.java.decomposition.graph.DirectedEdge;
+import main.java.decomposition.hyperGraph.Vertex;
+import main.java.decomposition.spqrTree.TCTree;
+import main.java.decomposition.spqrTree.TCTreeNode;
+
+public class STypeDetermination implements ITypeDetermination{
+
+
+
+
+    @Override
+    public void determineType(TCTree<DirectedEdge, Vertex> tcTree, TCTreeNode<DirectedEdge, Vertex> tcTreeNode) {
+
+        //TODO: implement method
+    }
+}
