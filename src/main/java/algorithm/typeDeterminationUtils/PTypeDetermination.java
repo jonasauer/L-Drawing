b8580355a6d48baa2feba420c612e9cc65d2323d
Jonas Auer <auerjonas95@gmail.com>
2018-01-16 00:28:55 +0100

    P Node should now be fully working, but i am not shure until R Node is fully working.

--- src/main/java/algorithm/typeDeterminationUtils/PTypeDetermination.java
+++ src/main/java/algorithm/typeDeterminationUtils/PTypeDetermination.java
@@ -8,6 +8,8 @@ import main.java.decomposition.spqrTree.TCTreeNode;
 import main.java.decomposition.spqrTree.TCTreeNodeType;
 import main.java.algorithm.holder.HolderProvider;
 
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -19,7 +21,6 @@ public class PTypeDetermination{
 
         TCTreeNode<DirectedEdge, Vertex> optTypeBNode = null;
         SuccessorPathType successorPathType = SuccessorPathType.TYPE_M;
-        MultiDirectedGraph augmentedGraph = HolderProvider.getAugmentationHolder().getAugmentedGraph();
 
         for(TCTreeNode<DirectedEdge, Vertex> child : tcTree.getChildren(tcTreeNode)){
 
@@ -32,6 +33,7 @@ public class PTypeDetermination{
         }
 
         HolderProvider.getSuccessorPathTypeHolder().getNodeTypes().put(tcTreeNode, successorPathType);
+        MultiDirectedGraph augmentedGraph = HolderProvider.getAugmentationHolder().getAugmentedGraph();
 
         Vertex source = HolderProvider.getSourceSinkPertinentGraphsHolder().getSourceNodes().get(tcTreeNode);
         Vertex target = HolderProvider.getSourceSinkPertinentGraphsHolder().getSinkNodes().get(tcTreeNode);
@@ -57,6 +59,41 @@ public class PTypeDetermination{
                 throw new RuntimeException("Type B and edge from source to sink is occurring on P-Node!");
 
             MultiDirectedGraph pertTypeB = HolderProvider.getPertinentGraphHolder().getPertinentGraphs().get(optTypeBNode);
+
+            //get successors/predecessors of the pert graph that causes the type B
+            List<Vertex> typeBSuccessorVertices = new LinkedList<>();
+            for(DirectedEdge edge : pertTypeB.getEdgesWithSource(source))
+                typeBSuccessorVertices.add(edge.getTarget());
+            List<Vertex> typeBPredecessorVertices = new LinkedList<>();
+            for(DirectedEdge edge : pertTypeB.getEdgesWithTarget(target)){
+                typeBPredecessorVertices.add(edge.getSource());
+            }
+
+            //get the corresponding edges
+            Collection<DirectedEdge> typeBSuccessorEdges = augmentedGraph.getEdgesWithTargets(typeBSuccessorVertices);
+            Collection<DirectedEdge> typeBPredecessorEdges = augmentedGraph.getEdgesWithSources(typeBPredecessorVertices);
+
+            //iterate over all outgoing edges and if it is part of the typeB pertGraph, remove it and add it again.
+            Iterator<DirectedEdge> edgeIterator = outgoingEdgesSource.iterator();
+            while (edgeIterator.hasNext()){
+                DirectedEdge edge = edgeIterator.next();
+                if(typeBSuccessorEdges.contains(edge)) {
+                    edgeIterator.remove();
+                    outgoingEdgesSource.add(edge);
+                }
+            }
+
+            //iterate over all incoming edges from right to left and if it is part of the typeB pertGraph, remove it and
+            //add it again on the insertIndex, which starts at zero (right path) and increases
+            int insertIndex = 0;
+            edgeIterator = incomingEdgesTarget.iterator();
+            while (edgeIterator.hasNext()){
+                DirectedEdge edge = edgeIterator.next();
+                if(typeBPredecessorEdges.contains(edge)) {
+                    edgeIterator.remove();
+                    incomingEdgesTarget.add(insertIndex++, edge);
+                }
+            }
         }
 
         //connect pertinentGraph of all children
--- src/main/java/algorithm/typeDeterminationUtils/RTypeDetermination.java
+++ src/main/java/algorithm/typeDeterminationUtils/RTypeDetermination.java
@@ -1,14 +1,18 @@
 package main.java.algorithm.typeDeterminationUtils;
 
+import main.java.algorithm.holder.HolderProvider;
+import main.java.algorithm.holder.SourceSinkPertinentGraphsHolder;
 import main.java.decomposition.graph.DirectedEdge;
+import main.java.decomposition.graph.MultiDirectedGraph;
 import main.java.decomposition.hyperGraph.Vertex;
+import main.java.decomposition.spqrTree.TCSkeleton;
 import main.java.decomposition.spqrTree.TCTree;
 import main.java.decomposition.spqrTree.TCTreeNode;
+import main.java.decomposition.spqrTree.TCTreeNodeType;
 
 public class RTypeDetermination{
 
     public static void determineType(TCTree<DirectedEdge, Vertex> tcTree, TCTreeNode<DirectedEdge, Vertex> tcTreeNode) {
-
-        //TODO: implement method
+        
     }
 }
