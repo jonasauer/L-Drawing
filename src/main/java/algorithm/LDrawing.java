f78f9bd6a4ef312dbf1471f360e2a121b7d9ce44
Jonas Auer <auerjonas95@gmail.com>
2018-01-15 01:35:04 +0100

    Improved P typedetermination, type b still not finished

--- src/main/java/algorithm/LDrawing.java
+++ src/main/java/algorithm/LDrawing.java
@@ -54,6 +54,7 @@ public class LDrawing {
                     break;
             }
         }
+        System.out.println(convertedGraph);
     }
 
 
--- src/main/java/algorithm/typeDeterminationUtils/PTypeDetermination.java
+++ src/main/java/algorithm/typeDeterminationUtils/PTypeDetermination.java
@@ -8,6 +8,7 @@ import main.java.decomposition.spqrTree.TCTreeNode;
 import main.java.decomposition.spqrTree.TCTreeNodeType;
 import main.java.algorithm.holder.HolderProvider;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class PTypeDetermination{
@@ -18,12 +19,13 @@ public class PTypeDetermination{
 
         TCTreeNode<DirectedEdge, Vertex> optTypeBNode = null;
         SuccessorPathType successorPathType = SuccessorPathType.TYPE_M;
+        MultiDirectedGraph augmentedGraph = HolderProvider.getAugmentationHolder().getAugmentedGraph();
 
         for(TCTreeNode<DirectedEdge, Vertex> child : tcTree.getChildren(tcTreeNode)){
 
             if(HolderProvider.getSuccessorPathTypeHolder().getNodeTypes().get(child).equals(SuccessorPathType.TYPE_B)){
                 if(optTypeBNode != null)
-                    throw new RuntimeException("Type M is occurring twice in P-Node!");
+                    throw new RuntimeException("Type B is occurring twice in P-Node!");
                 optTypeBNode = child;
                 successorPathType = SuccessorPathType.TYPE_B;
             }
@@ -31,11 +33,9 @@ public class PTypeDetermination{
 
         HolderProvider.getSuccessorPathTypeHolder().getNodeTypes().put(tcTreeNode, successorPathType);
 
-
-
         Vertex source = HolderProvider.getSourceSinkPertinentGraphsHolder().getSourceNodes().get(tcTreeNode);
         Vertex target = HolderProvider.getSourceSinkPertinentGraphsHolder().getSinkNodes().get(tcTreeNode);
-        DirectedEdge sourceSinkEdge = HolderProvider.getAugmentationHolder().getAugmentedGraph().getEdge(source, target);
+        DirectedEdge sourceSinkEdge = augmentedGraph.getEdge(source, target);
         List<DirectedEdge> outgoingEdgesSource = HolderProvider.getEmbeddingHolder().getOutgoingEdgesCircularOrdering(source);
         List<DirectedEdge> incomingEdgesTarget = HolderProvider.getEmbeddingHolder().getIncomingEdgesCircularOrdering(target);
 
@@ -51,27 +51,22 @@ public class PTypeDetermination{
                 incomingEdgesTarget.remove(sourceSinkEdge);
                 incomingEdgesTarget.add(0, sourceSinkEdge);
             }
-
-            //connect pertinentGraph of all children
-            int targetPosition = 0;
-            for(TCTreeNode<DirectedEdge, Vertex> child : tcTree.getChildren(tcTreeNode)){
-
-                int amountOfSuccessorVertices = HolderProvider.getPertinentGraphHolder()
-                        .getPertinentGraphs().get(child).getEdges(source).size();
-                targetPosition += amountOfSuccessorVertices;
-
-                //add the connection between the two children
-                if(targetPosition < outgoingEdgesSource.size()){
-                    Vertex connectionSource = outgoingEdgesSource.get(targetPosition-1).getTarget();
-                    Vertex connectionTarget = outgoingEdgesSource.get(targetPosition  ).getTarget();
-                    DirectedEdge augmentedEdge = HolderProvider.getAugmentationHolder().getAugmentedGraph().addEdge(connectionSource, connectionTarget);
-                    HolderProvider.getAugmentationHolder().getAugmentedEdges().add(augmentedEdge);
-                }
-            }
-
         }else{
-            //TODO: place the type B node on the right, connect all last successors with the following successors of the
-            //TODO: source of the current pert graph and the type B node with the last type M node.
+
+            if(sourceSinkEdge != null)
+                throw new RuntimeException("Type B and edge from source to sink is occurring on P-Node!");
+
+            MultiDirectedGraph pertTypeB = HolderProvider.getPertinentGraphHolder().getPertinentGraphs().get(optTypeBNode);
+        }
+
+        //connect pertinentGraph of all children
+        for(int i = 0; i < outgoingEdgesSource.size()-1; i++){
+            Vertex firstSuccessor = outgoingEdgesSource.get(i).getTarget();
+            Vertex secondSuccessor = outgoingEdgesSource.get(i+1).getTarget();
+            if(augmentedGraph.getEdge(firstSuccessor, secondSuccessor) == null){
+                DirectedEdge augmentedEdge = augmentedGraph.addEdge(firstSuccessor, secondSuccessor);
+                HolderProvider.getAugmentationHolder().getAugmentedEdges().add(augmentedEdge);
+            }
         }
     }
 }
