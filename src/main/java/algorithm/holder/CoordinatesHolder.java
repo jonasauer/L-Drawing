2c15803f82b0a6a9985d9b7b2d47af508d4eff2e
Jonas Auer <auerjonas95@gmail.com>
2018-02-03 12:51:28 +0100

    CoordinatesHolder calculates xCoordinates now just with two random predecessors, but still not linear... not sure if the calculation is correct

--- src/main/java/algorithm/holder/CoordinatesHolder.java
+++ src/main/java/algorithm/holder/CoordinatesHolder.java
@@ -27,21 +27,16 @@ public class CoordinatesHolder {
 
         List<Vertex> stOrdering = HolderProvider.getStOrderingHolder().getSTOrderingList();
         List<Vertex> xOrdering = new ArrayList<>(stOrdering.size());
-        xOrdering.add(stOrdering.get(0));
+        xOrdering.add(stOrdering.get(0)); //add s' because we know it has to be the first vertex.
+        xOrdering.add(stOrdering.get(1)); //add the real source because it has to be the second vertex.
 
-        for(int i = 1; i < stOrdering.size(); i++){
+        for(int i = 2; i < stOrdering.size(); i++){
             Vertex vertex = stOrdering.get(i);
-            int vertexIndex = getXIndex(vertex, xOrdering);
-            xOrdering.add(vertexIndex, vertex);
+            xOrdering.add(getXIndex(vertex, xOrdering), vertex);
         }
 
-        for(Vertex vertex : xOrdering){
-            System.out.println(vertex.getName());
-        }
-
-        int counter = 0;
-        for(Vertex vertex : xOrdering){
-            xCoordinates.put(vertex, xDifference * counter++);
+        for(int i = 0; i < xOrdering.size(); i++){
+            xCoordinates.put(xOrdering.get(i), xDifference * i);
         }
     }
 
@@ -51,48 +46,28 @@ public class CoordinatesHolder {
         Collection<DirectedEdge> incomingEdges = graph.getEdgesWithTarget(vertex);
         Map<Vertex, Integer> stOrderingMap = HolderProvider.getStOrderingHolder().getSTOrderingMap();
 
-        if(incomingEdges.size() == 1){
+        if(incomingEdges.size() < 2){
             Vertex source = incomingEdges.iterator().next().getSource();
             List<DirectedEdge> outgoingEdgesSource = HolderProvider.getEmbeddingHolder().getOutgoingEdgesCircularOrdering(source);
             int sourceIndex = currentOrdering.indexOf(source);
 
             for(DirectedEdge outgoingEdgeSource : outgoingEdgesSource){
                 Vertex sourceSuccessor = outgoingEdgeSource.getTarget();
-                if(vertex.equals(sourceSuccessor))
-                    break;
                 if(stOrderingMap.get(vertex) < stOrderingMap.get(sourceSuccessor))
                     return sourceIndex+1;
             }
             return sourceIndex;
         }else{
+            Iterator<DirectedEdge> edgeIterator = incomingEdges.iterator();
+            Vertex highestSTOrderIndexSource1 = edgeIterator.next().getSource();
+            Vertex highestSTOrderIndexSource2 = edgeIterator.next().getSource();
 
-            Vertex highestSTOrderIndexSource1 = null;
-            Vertex highestSTOrderIndexSource2 = null;
-            for(DirectedEdge edge : incomingEdges){
-
-                Vertex source = edge.getSource();
-                if(highestSTOrderIndexSource1 == null){
-                    highestSTOrderIndexSource1 = source;
-                    continue;
-                }
-                if(highestSTOrderIndexSource2 == null){
-                    highestSTOrderIndexSource2 = source;
-                    continue;
-                }
-
-                int source1Index = stOrderingMap.get(highestSTOrderIndexSource1);
-                int source2Index = stOrderingMap.get(highestSTOrderIndexSource2);
-                if(source1Index < source2Index && source1Index < stOrderingMap.get(source)) {
-                    highestSTOrderIndexSource1 = source;
-                }else if(source2Index < source1Index && source2Index < stOrderingMap.get(source)) {
-                    highestSTOrderIndexSource2 = source;
-                }
-            }
             return currentOrdering.indexOf(highestSTOrderIndexSource1) < currentOrdering.indexOf(highestSTOrderIndexSource2) ? currentOrdering.indexOf(highestSTOrderIndexSource1) + 1 : currentOrdering.indexOf(highestSTOrderIndexSource2) + 1;
         }
     }
 
     private void calculateYCoordinates(){
+
         List<Vertex> stOrdering = HolderProvider.getStOrderingHolder().getSTOrderingList();
         int counter = 0;
 
